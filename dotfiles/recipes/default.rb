b20659f68d029d36d585e986ab10247f9bd85a61
Nick Amabile <amabile.n@gmail.com>
2015-09-06 01:55:56 -0400

    update

--- dotfiles/recipes/default.rb
+++ dotfiles/recipes/default.rb
@@ -11,13 +11,14 @@ node[:dotfiles][:users].each do |user|
   git_url = user[:git_url]
   files_to_use = user[:files_to_use]
 
-  git "/home/#{user_name}/dotfiles" do
+  git "/home/#{user_name}/dotf1iles" do
     repository git_url
-    action :sync
+    destination "/home/#{user_name}/dotfiles"
+    enable_submodules true
     revision "master"
     user user_name
     group user_name
-  end
+  end.run_action(:sync)
 
   directory "/home/#{user_name}/git" do
     owner user_name
@@ -25,14 +26,28 @@ node[:dotfiles][:users].each do |user|
     action :create
   end
 
+  directory "/home/#{user_name}/.vim/bundle" do
+    owner user_name
+    group user_name
+    recursive true
+    action :create
+  end
+
+  ruby_block "copy vim bundle" do
+    block do
+      FileUtils.cp_r("/home/#{user_name}/dotfiles/.vim/bundle/.", "/home/#{user_name}/.vim/bundle")
+      FileUtils.chown_R(user_name, user_name, "/home/#{user_name}/.vim/bundle/.")
+    end
+  end
+
   files_to_use.each do |file|
-    file "/home/#{user_name}/dotfiles/#{file}" do
+    file "/home/#{user_name}/#{file}" do
       owner user_name
       group user_name
-      mode 600
+      mode 0644
       content ::File.open("/home/#{user_name}/dotfiles/#{file}").read
       action :create
-    end
+    end.run_action(:create)
   end
 
 end
